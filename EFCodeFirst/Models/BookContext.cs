f8f5298fd4878df6d928ab7526e47db3ed564ea5
ColbyAJ03 <alex.colby3@gmail.com>
2017-06-02 09:16:39 -0700

    Code First exercise
    simple book inventory manager created with entity framework code first approach

---
+++ EFCodeFirst/Models/BookContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EFCodeFirst.Models
+{
+    public class BookContext : DbContext
+
+    {
+        public BookContext() 
+            : base("name=BookContext")
+        { }
+
+        public virtual DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().HasKey(x => x.Id);
+            modelBuilder.Entity<Book>().Property(x => x.Id)
+                .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
